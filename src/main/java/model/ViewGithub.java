a82f1859be83e360fc92fa657b510a18ffe31ecd
WhiteZane <whi12059@byui.edu>
2017-03-31 20:15:00 -0600

    3/31/2016 - "added badlogin.JSP, fixed login redirect profile w3"

--- src/main/java/model/ViewGithub.java
+++ src/main/java/model/ViewGithub.java
@@ -9,7 +9,11 @@ import com.jcabi.github.Coordinates;
 import com.jcabi.github.RtGithub;
 import com.jcabi.github.Issue;
 import com.jcabi.github.Github;
+import com.jcabi.github.Issues;
 import com.jcabi.github.Repo;
+import com.jcabi.github.Repos;
+import com.jcabi.github.User;
+import com.jcabi.http.Request;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.logging.Level;
@@ -20,18 +24,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 /**
  *
  * @author ghosty
  */
 @WebServlet(name = "ViewGithub", urlPatterns = {"/ViewGithub"})
 public class ViewGithub extends HttpServlet {
-    
+
     public static void github(String[] args) {
-    Github github = new RtGithub("d4001490201c5c5ad6d87f1834ac523d616c33a3");
-    Repo repo = github.repos().get(new Coordinates.Simple("jcabi", "jcabi-github"));
-    Issue issue = null;
+        Github github = new RtGithub("d4001490201c5c5ad6d87f1834ac523d616c33a3");
+        Repos repos = github.repos();
+        Repo repo = repos.get(new Coordinates.Simple("WhiteZane/CS313GroupProject"));
+        Issue issue = null;
         try {
             issue = repo.issues().create("How are you?", "Please tell me...");
         } catch (IOException ex) {
@@ -42,7 +46,7 @@ public class ViewGithub extends HttpServlet {
         } catch (IOException ex) {
             Logger.getLogger(ViewGithub.class.getName()).log(Level.SEVERE, null, ex);
         }
-  }
+    }
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -55,15 +59,14 @@ public class ViewGithub extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-        
+
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet View Github</title>");            
+            out.println("<title>Servlet View Github</title>");
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet ViewGithub at " + request.getContextPath() + "</h1>");
